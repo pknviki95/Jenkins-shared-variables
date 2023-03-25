47194ac356a2e20912b56ad1eb65eeb90b6ba459
pknviki95 <55426296+pknviki95@users.noreply.github.com>
2023-03-26 00:00:16 +0530

    Update CheckoutSCM.groovy

--- vars/CheckoutSCM.groovy
+++ vars/CheckoutSCM.groovy
@@ -1,4 +1,4 @@
 def call (Map git_variable = [:])
 {
-  sh 'git branch:'main',url:'https://github.com/pknviki95/Jenkinsfile_pipeline_SCM.git''
+  sh "git branch:'main',url:'https://github.com/pknviki95/Jenkinsfile_pipeline_SCM.git'"
     }
